7f84df694edfa21d4cf278dad7be5b104dbda271
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-10 09:15:55 +0500

    add logic for completed task

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -4,10 +4,12 @@ import { Todo } from '../types';
 
 type TodoList = {
 	todos: Todo[];
+	completeTodos: Todo[];
 };
 
 const initialState: TodoList = {
 	todos: [],
+	completeTodos: [],
 };
 
 const todoSlice = createSlice({
@@ -19,8 +21,23 @@ const todoSlice = createSlice({
 		},
 
 		completeTodo(state, action: PayloadAction<string>) {
-			const checkedTodo = state.todos.find(el => el.id === action.payload);
-			if (checkedTodo) checkedTodo.completed = !checkedTodo.completed;
+			const todoId = action.payload;
+			const todoToComplete = state.todos.find(todo => todo.id === todoId);
+
+			if (todoToComplete) {
+				todoToComplete.completed = !todoToComplete.completed;
+
+				// Проверяем, completed === true или false
+				if (todoToComplete.completed) {
+					// Перемещаем задачу в completeTodos
+					state.completeTodos.push(todoToComplete);
+					state.todos = state.todos.filter(todo => todo.id !== todoId);
+				} else {
+					// Перемещаем задачу обратно в todos
+					state.todos.push(todoToComplete);
+					state.completeTodos = state.completeTodos.filter(todo => todo.id !== todoId);
+				}
+			}
 		},
 	},
 });
