79f4c17a3a0abc0738cff3fc4a98155c4ae5f8d4
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-11 10:09:21 +0500

    to do slice done

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '.';
 import { Todo } from '../types';
-import { updateCoinsAndExp } from './userSlice';
 
 type TodoList = {
 	todos: Todo[];
