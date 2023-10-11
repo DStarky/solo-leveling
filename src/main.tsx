2d13d6e9a7b99e801f28a5eeca7ba6edab5fb5b4
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-11 12:27:01 +0500

    local storage work

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store'
-import Routing from './Routing'
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store, { persistor } from './store';
+import Routing from './Routing';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './styles/main.scss';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <Routing />
-  </Provider>
-)
+	<PersistGate
+		loading={null}
+		persistor={persistor}>
+		<Provider store={store}>
+			<Routing />
+		</Provider>
+	</PersistGate>,
+);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './userSlice';
 import todoReducer from './todoSlice';
 
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const rootReducer = combineReducers({
+	user: userReducer,
+	todo: todoReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-	reducer: {
-		user: userReducer,
-		todo: todoReducer,
-	},
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
